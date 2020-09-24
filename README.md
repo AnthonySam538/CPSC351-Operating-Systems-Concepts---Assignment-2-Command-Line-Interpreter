081958ae3c4ef889b113c48343dc99db5b0fbfbc
AnthonySam538 <42751473+AnthonySam538@users.noreply.github.com>
2020-09-24 16:56:08 -0700

    Good enough, I'd say.
    Phew! Tough one.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# cs351_assignment2_anthony_sam
-Interactive Shell
+# cs351_assignment2_anthony_sam: Interactive Shell
+This shell supports the following commands:
+* dir
+* help
+* vol
+* path
+* tracklist
+* notepad
+* echo
+* color
+* ping
+* exit
+* quit
+When the user types in a command, a thread is created to execute it.
+
+## Known issue(s):
+The rest of the program doesn't wait for the thread to finish executing the command.
--- cs351_assignment2_anthony_sam.cpp
+++ cs351_assignment2_anthony_sam.cpp
@@ -1,33 +1,73 @@
 // cs351_assignment2_anthony_sam.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // Copyright (c) Anthony Sam 9/24/2020
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
 #include <stdlib.h>     /* system, NULL, EXIT_FAILURE */
+#include <windows.h>
+
+DWORD __stdcall executeCommand(LPVOID lpParameter)
+{
+    // synthesize the command together and execute it
+    std::string command = ((std::string*)lpParameter)[0] + " " + ((std::string*)lpParameter)[1] + " " + ((std::string*)lpParameter)[2] + " " + ((std::string*)lpParameter)[3];
+    system(&command[0]);
+
+    // clear the array now that the command has been executed
+    ((std::string*)lpParameter)[0] = "";
+    ((std::string*)lpParameter)[1] = "";
+    ((std::string*)lpParameter)[2] = "";
+    ((std::string*)lpParameter)[3] = "";
+
+    return 0;
+}
 
 int main()
 {
-    std::string userInput;
+    std::string userInput; // the user's input will be stored here
+    std::string argument[] = {"", "", "", ""}; // the components of the user's input will be stored here
+    char* pch; // used in the strtok() function
+    short currentIndex; // used in filling out argument[]
+    DWORD dwThreadId; // used in CreateProcess()
 
-    std::cout << "Welcome to myShell!\n\n";
+    std::cout << "Welcome to myShell!\n\n"; // display an introductory message
 
     while (true)
     {
-        std::cout << "Enter a command: ";
-        std::getline(std::cin, userInput);
-        system(&userInput[0]);
-        if (userInput == "exit")
+        currentIndex = 0; // reset currentIndex
+
+        //std::cout << "Current contents: \n" << argument[0] << std::endl << argument[1] << std::endl << argument[2] << std::endl << argument[3] << std::endl;
+
+        std::cout << ">>> "; // signal the user that the program is ready for input
+        std::getline(std::cin, userInput); // receive the user's input
+
+        // fill the array with the user's input using the strtok() command
+        pch = strtok(&userInput[0], " ");
+        while (pch != nullptr)
+        {
+            argument[currentIndex] = pch;
+            pch = strtok(nullptr, " ");
+            ++currentIndex;
+        }
+        
+        // use the user's input
+        if (argument[0] == "exit" || argument[0] == "quit")
+        {
+            std::cout << "Now leaving myShell...";
+
             return 0;
+        }
+        else if (argument[0] == "dir" || argument[0] == "help" || argument[0] == "vol" || argument[0] == "path" || argument[0] == "tasklist" || argument[0] == "notepad" || argument[0] == "echo" || argument[0] == "color" || argument[0] == "ping")
+        {
+            CreateThread(
+                NULL,            // default security attributes
+                0,               // use default stack size
+                executeCommand, // thread function name (If you want to see the function definition for executeCommand, scroll up)
+                &argument,       // argument to thread function
+                0,               // use default creation flags
+                &dwThreadId);    // returns the thread identifier
+        }
+        else
+            std::cout << argument[0] << " isn't a recognized command.\n";
     }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
